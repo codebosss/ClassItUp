875048ac833b0c8c30e4ccc06ef9e03390878924
codebosss <88897108+codebosss@users.noreply.github.com>
2021-11-15 13:49:31 +0530

    Add routes

---
+++ server/routes/analysis.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import { startInterviewAnalysis, InterviewAnalysisResult, stopInterviewAnalysis } from '../controllers/api/symbl/InterviewAnalysis/zoomApi.js'
+import { sendVideoData } from '../controllers/api/symbl/PitchAnalysis/asyncApi.js'
+import { getDataToCompare, getConversationList, fetchAnanlysisData } from '../controllers/analysisData.js'
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+
+router.get('/stopInterviewAnalysis/:connectionId', auth, stopInterviewAnalysis)
+router.post('/interviewAnalysisResult', auth, InterviewAnalysisResult)
+router.post('/interviewAnalysis', auth, startInterviewAnalysis);
+
+//------------Interaction Analysis routes------------//
+router.post('/pitchAnalysis/sendVideoData', auth, sendVideoData);
+router.get('/pitchAnalysis/getConversationList/', auth, getConversationList);
+
+//------------Dashboard routes------------//
+router.get('/pitchAnalysis/getComparisonData/:conversationId1/:conversationId2', auth, getDataToCompare)
+router.get('/pitchAnalysis/fetchAnalysisResult/:conversationId', auth, fetchAnanlysisData)
+
+
+export default router;
