d10c91af7ddfbb9686f4a4242708c2631b0e64f4
codebosss <88897108+codebosss@users.noreply.github.com>
2021-11-15 13:54:57 +0530

    Add services

---
+++ server/controllers/notification/notificationService.js
@@ -0,0 +1,71 @@
+import request from 'request';
+import User from '../../models/user.js';
+
+export const addUserToSMTP = (options) => {
+    return new Promise(async (resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) throw new Error(error);
+            console.log(body);
+            try {
+                resolve(body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+export const sendEmail = (options) => {
+    return new Promise(async (resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) throw new Error(error);
+            console.log(body, "email");
+            try {
+                resolve(body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+
+export const sendSms = (options) => {
+    return new Promise(async (resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) throw new Error(error);
+            console.log(body, "sms");
+            try {
+                resolve(body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+
+export const sendViaWhatsapp = (options) => {
+    return new Promise(async (resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) throw new Error(error);
+            console.log(body, 'whatsapp');
+            try {
+                resolve(body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+export const findStudents = (filter) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const data = await User.find(filter)
+            resolve(data);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
