5abd48ca860261e586c7245b569e945166cf40d2
codebosss <88897108+codebosss@users.noreply.github.com>
2021-11-15 14:12:33 +0530

    Add index and styles

---
+++ client/src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    bottom: '0',
+    padding: '0',
+    margin: '0',
+  },
+}));
+
+export default useStyles;
